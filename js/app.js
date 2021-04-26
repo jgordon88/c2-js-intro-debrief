7b876ada2df9d5fec488db1669dde164e9c7a0b1
James <jamesogordon0@gmail.com>
2021-04-26 10:07:58 -0500

    Completed Assignment

---
+++ js/app.js
@@ -0,0 +1,44 @@
+//genBuzz
+
+// let myNumber = 3
+
+// if (typeof myNumber !== 'number'){
+//   console.log('This is not a number')
+// } else if (myNumber % 3 === 0) { 
+//   console.log('Gen')
+// } else if (myNumber % 5 === 0) { 
+//   console.log('Buzz')
+// } else if (myNumber % 15 === 0) {
+//   console.log('GenBuzz')
+// } else {
+//   console.log('myNumber');
+// }
+
+//eCommerce
+
+// let item = 'shoes'; 
+
+// switch (item) {
+//   case 'Shoes':
+//     console.log('Shoes are $50');
+//     break;
+//   case 'Jeans':
+//     console.log('Jeans are $25')
+//     break;
+//   case 'Hat':
+//     console.log('Hat is $12')
+//     break;
+//   case 'Socks':
+//     console.log('Socks are $2')
+//     break;
+//   default:
+//     console.log('Invalid Item')
+//     break;
+// } 
+
+//Print a random integer
+
+// let min = 50;
+// let max =100;
+
+// console.log(math.floor(math.random()* (max-min) + min));
